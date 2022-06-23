441e2af462401dc0cfc7d99ec7e03c929214255e
0lru <0lru@gmx.de>
2022-06-24 01:49:14 +0200

    run in executor

--- p3ui/__init__.py
+++ p3ui/__init__.py
@@ -1,7 +1,10 @@
+from asyncio import futures
+
 from p3ui.native import *
 from .matplotlib_surface import MatplotlibSurface
 from .mpl import pixels_to_points, points_to_pixels, dpi
 import p3ui.skia as skia
+import concurrent.futures
 
 import asyncio
 
@@ -12,9 +15,10 @@ class GuiEventLoop(asyncio.AbstractEventLoop):
         super().__init__(*args, **kwargs)
         self.__native_event_loop = EventLoop()
         self._running = False
+        self._default_executor = None
 
     def get_debug(self):
-        """ dunno """
+        """ leftover?! """
         return False
 
     @property
@@ -63,7 +67,7 @@ class GuiEventLoop(asyncio.AbstractEventLoop):
 
     def call_later(self, delay, callback, *args):
         #
-        # TODO: use decent time representation
+        # TODO: use more decent time representation
         return self.call_at(self.time + delay, callback, *args)
 
     def call_at(self, when, callback, *args):
@@ -78,6 +82,21 @@ class GuiEventLoop(asyncio.AbstractEventLoop):
     def create_future(self):
         return asyncio.Future(loop=self)
 
+    def run_in_executor(self, executor, func, *args):
+        # if self.closed:
+        #    raise RuntimeError('...')
+        if executor is None:
+            executor = self._default_executor
+            if executor is None:
+                executor = concurrent.futures.ThreadPoolExecutor(thread_name_prefix='p3ui')
+                self._default_executor = executor
+        return futures.wrap_future(executor.submit(func, *args), loop=self)
+
+    def shutdown_default_executor(self):
+        if self._default_executor is None:
+            return
+        self._default_executor.shutdown(wait=True)
+
 
 async def __entry_task(loop, entry_function):
     await entry_function
@@ -90,3 +109,4 @@ def run(entry_function):
     asyncio._set_running_loop(loop)
     loop.create_task(__entry_task(loop, entry_function))
     loop.run_forever()
+    loop.shutdown_default_executor()
