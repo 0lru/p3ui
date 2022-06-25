2c35c82d370d8134c0fe2d8568b9e72612add44f
0lru <0lru@gmx.de>
2022-06-25 11:12:37 +0200

    color to skia color

--- cpp/p3/source/p3/Context.h
+++ cpp/p3/source/p3/Context.h
@@ -2,57 +2,55 @@
 
 #include "StyleTypes.h"
 
-#include <memory>
 #include <array>
+#include <functional>
+#include <memory>
 #include <optional>
 #include <string>
-#include <functional>
 
-namespace p3
-{
+namespace p3 {
 
-    class Theme;
-    class UserInterface;
-    class RenderBackend;
-    class RenderLayer;
-    class RenderTarget;
+class Theme;
+class UserInterface;
+class RenderBackend;
+class RenderLayer;
+class RenderTarget;
 
-    class Context : public std::enable_shared_from_this<Context>
-    {
-    public:
-        using MouseMove = std::optional<std::array<float, 2>>;
-        using Postponed = std::function<void()>;
+class Context : public std::enable_shared_from_this<Context> {
+public:
+    using MouseMove = std::optional<std::array<float, 2>>;
+    using Postponed = std::function<void()>;
 
-        Context(UserInterface&, RenderBackend&, MouseMove);
-        ~Context();
+    Context(UserInterface&, RenderBackend&, MouseMove);
+    ~Context();
 
-        static Context& current();
+    static Context& current();
 
-        UserInterface& user_interface() const;
-        RenderBackend& render_backend() const;
+    UserInterface& user_interface() const;
+    RenderBackend& render_backend() const;
 
-        float rem() const;
-        Theme& theme() const;
-        
-        MouseMove const& mouse_move() const;
-        float to_actual(Length const&) const;
+    float rem() const;
+    Theme& theme() const;
 
-        RenderLayer& render_layer() const;
-        void push_render_layer(RenderLayer& layer) { _render_layer.push_back(&layer); }
-        void pop_render_layer() { _render_layer.pop_back(); }
-        void set_render_target(RenderTarget* render_target) { _render_target = render_target; }
-        RenderTarget* render_target() const { return _render_target; }
+    MouseMove const& mouse_move() const;
+    float to_actual(Length const&) const;
 
-        void set_show_render_layers(bool show_render_layers) { _show_render_layers = show_render_layers; }
-        bool show_render_layers() const { return _show_render_layers; }
+    RenderLayer& render_layer() const;
+    void push_render_layer(RenderLayer& layer) { _render_layer.push_back(&layer); }
+    void pop_render_layer() { _render_layer.pop_back(); }
+    void set_render_target(RenderTarget* render_target) { _render_target = render_target; }
+    RenderTarget* render_target() const { return _render_target; }
 
-    private:
-        bool _show_render_layers = true;
-        UserInterface& _user_interface;
-        RenderBackend& _render_backend;
-        MouseMove _mouse_move;
-        RenderTarget* _render_target;
-        std::vector<RenderLayer*> _render_layer;
-    };
+    void set_show_render_layers(bool show_render_layers) { _show_render_layers = show_render_layers; }
+    bool show_render_layers() const { return _show_render_layers; }
+
+private:
+    bool _show_render_layers = true;
+    UserInterface& _user_interface;
+    RenderBackend& _render_backend;
+    MouseMove _mouse_move;
+    RenderTarget* _render_target;
+    std::vector<RenderLayer*> _render_layer;
+};
 
 }
--- cpp/p3_python/source/p3/python/Color.cpp
+++ cpp/p3_python/source/p3/python/Color.cpp
@@ -4,78 +4,76 @@
 
 #include <fmt/format.h>
 
+namespace p3::python {
 
-namespace p3::python
+template <typename T>
+std::uint8_t color_component(T& object)
 {
+    return py::isinstance<std::uint8_t>(object)
+        ? object.cast<std::uint8_t>()
+        : std::uint8_t(object.cast<double>() * 255.);
+}
 
-    template<typename T>
-    std::uint8_t color_component(T& object)
-    {
-        return py::isinstance<std::uint8_t>(object)
-            ? object.cast<std::uint8_t>()
-            : std::uint8_t(object.cast<double>() * 255.);
-    }
-
-    void Definition<Color>::apply(py::module& module)
-    {
-        py::class_<Color> color(module, "Color");
-        color.def(py::init<>());
-        color.def(py::init<>([](py::tuple tuple) {
-            return Color(
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[0]) 
-                    ? tuple[0].cast<float>()*255.f 
-                    : tuple[0].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[1]) 
-                    ? tuple[1].cast<float>()*255.f 
-                    : tuple[1].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[2]) 
-                    ? tuple[2].cast<float>()*255.f 
-                    : tuple[2].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[3]) 
-                    ? tuple[3].cast<float>()*255.f 
+void Definition<Color>::apply(py::module& module)
+{
+    py::class_<Color> color(module, "Color");
+    color.def(py::init<>());
+    color.def(py::init<>([](py::tuple tuple) {
+        return Color(
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[0])
+                    ? tuple[0].cast<float>() * 255.f
+                    : tuple[0].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[1])
+                    ? tuple[1].cast<float>() * 255.f
+                    : tuple[1].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[2])
+                    ? tuple[2].cast<float>() * 255.f
+                    : tuple[2].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(tuple[3])
+                    ? tuple[3].cast<float>() * 255.f
                     : tuple[3].cast<std::uint8_t>()));
-        }));
-        color.def(py::init<>([](py::list l) {
-            return Color(
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(l[0]) 
-                    ? l[0].cast<float>()*255.f 
-                    : l[0].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(l[1]) 
-                    ? l[1].cast<float>()*255.f 
-                    : l[1].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(l[2]) 
-                    ? l[2].cast<float>()*255.f 
-                    : l[2].cast<std::uint8_t>()), 
-                static_cast<std::uint8_t>(py::isinstance<py::float_>(l[3]) 
-                    ? l[3].cast<float>()*255.f 
+    }));
+    color.def(py::init<>([](py::list l) {
+        return Color(
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(l[0])
+                    ? l[0].cast<float>() * 255.f
+                    : l[0].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(l[1])
+                    ? l[1].cast<float>() * 255.f
+                    : l[1].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(l[2])
+                    ? l[2].cast<float>() * 255.f
+                    : l[2].cast<std::uint8_t>()),
+            static_cast<std::uint8_t>(py::isinstance<py::float_>(l[3])
+                    ? l[3].cast<float>() * 255.f
                     : l[3].cast<std::uint8_t>()));
-        }));
-        color.def(py::init<std::uint32_t>());
-        color.def(py::init<std::string>());
-        color.def(py::init<std::uint8_t, std::uint8_t, std::uint8_t, std::uint8_t>());
-        color.def("__repr__", [](Color const& color) {
-            return fmt::format("[{}, {}, {}, {}]", color.red(), color.green(), color.blue(), color.alpha());
-        });
-        color.def_property("red", &Color::red, &Color::set_red);
-        color.def_property("green", &Color::green, &Color::set_green);
-        color.def_property("blue", &Color::blue, &Color::set_blue);
-        color.def_property("alpha", &Color::alpha, &Color::set_alpha);
-        color.def_property_readonly("rgb", [](Color const& color) {
-            return std::array<std::uint8_t, 3>{color.red(), color.green(), color.blue()};
-        });
-        color.def_property_readonly("rgba", [](Color const& color) {
-            return std::array<std::uint8_t, 4>{color.red(), color.green(), color.blue(), color.alpha()};
-        });
-        color.def("__int__", [](Color const& color) {
-            return color.operator uint32_t();
-        });
-
-        // tuple -> color
-        py::implicitly_convertible<py::tuple, Color>();
-        py::implicitly_convertible<py::list, Color>();
-        py::implicitly_convertible<std::string, Color>();
-    }
-
-
+    }));
+    color.def(py::init<std::uint32_t>());
+    color.def(py::init<std::string>());
+    color.def(py::init<std::uint8_t, std::uint8_t, std::uint8_t, std::uint8_t>());
+    color.def("__repr__", [](Color const& color) {
+        return fmt::format("[{}, {}, {}, {}]", color.red(), color.green(), color.blue(), color.alpha());
+    });
+    color.def_property("red", &Color::red, &Color::set_red);
+    color.def_property("green", &Color::green, &Color::set_green);
+    color.def_property("blue", &Color::blue, &Color::set_blue);
+    color.def_property("alpha", &Color::alpha, &Color::set_alpha);
+    color.def_property_readonly("rgb", [](Color const& color) {
+        return std::array<std::uint8_t, 3> { color.red(), color.green(), color.blue() };
+    });
+    color.def_property_readonly("rgba", [](Color const& color) {
+        return std::array<std::uint8_t, 4> { color.red(), color.green(), color.blue(), color.alpha() };
+    });
+    color.def("__int__", [](Color const& color) {
+        return color.operator uint32_t();
+    });
+    color.def_property_readonly("skia", [](Color const& color) {
+        return std::uint32_t(color.alpha() << 24 | color.blue() << 16 | color.green() << 8 | color.red() << 0);
+    });
+    // tuple -> color
+    py::implicitly_convertible<py::tuple, Color>();
+    py::implicitly_convertible<py::list, Color>();
+    py::implicitly_convertible<std::string, Color>();
+}
 
 }
